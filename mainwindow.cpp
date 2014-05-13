0753f7e33865c561d75380b51cb94fcac60b3819
Katherine Whitlock <toroidalcode@gmail.com>
2014-05-13 13:37:07 -0400

    Hashing

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -65,7 +65,7 @@ using namespace std;
 #define PORT 7681
 
 double gInOutScaler = 1.0;
-#define CONVERT_IN_TO_OUT(in)  ((float) ((in) * gInOutScaler))
+#define CONVERT_IN_TO_OUT(in) ((float)((in) * gInOutScaler))
 
 /* This routine will be called by the PortAudio engine when audio is needed.
 ** It may be called at interrupt level on some machines so don't do anything
@@ -75,40 +75,42 @@ static fftwf_complex left_out[FRAMES_PER_BUFFER], right_out[FRAMES_PER_BUFFER];
 static fftwf_plan lp, rp;
 static float *left_mid;
 static float *right_mid;
-mutex plan_mtx;           // mutex for plans
 mutex mid_mtx;
 mutex out_mtx;
 
-bool plan_set;                 // indicates if plans exist
 mutex matrix_mtx;
 
+// 1/3 octave middle frequency array
+static const float omf[] = { 15.6, 31.3, 62.5, 125,  250,  500,
+                             1000, 2000, 4000, 8000, 16000 };
 
 // 1/3 octave middle frequency array
-static const float omf[] =
-{ 15.6,  31.3, 62.5, 125, 250, 500, 1000, 2000, 4000, 8000, 16000 };
+static const float tomf[] = { 15.6,    19.7,   24.8,   31.3,   39.4,    49.6,
+                              62.5,    78.7,   99.2,   125.0,  157.5,   198.4,
+                              250,     315,    396.9,  500.0,  630,     793.7,
+                              1000.0,  1259.9, 1587.4, 2000,   2519.8,  3174.8,
+                              4000.0,  5039.7, 6349.6, 8000.0, 10079.4, 12699.2,
+                              16000.0, 20158.7 };
 
+static float lbtomf[32] = { 0 };
+static float ubtomf[32] = { 0 };
 
-// 1/3 octave middle frequency array
-static const float tomf[] =
-{ 15.6, 19.7, 24.8, 31.3, 39.4, 49.6, 62.5, 78.7, 99.2,
-  125.0, 157.5, 198.4, 250, 315, 396.9, 500.0, 630, 793.7,
-  1000.0, 1259.9, 1587.4, 2000, 2519.8, 3174.8, 4000.0, 5039.7,
-  6349.6, 8000.0, 10079.4, 12699.2, 16000.0, 20158.7};
-
-static float lbtomf[32] = {0};
-static float ubtomf[32] = {0};
-
-template <typename T, class C>
-inline T average(C &c) {
-    float a = 0;
-    float scalar = (c.size() / 48.0);
-    for (auto b : c) {
-        a += b * (scalar < 1 ? 1 : scalar);
-    }
+template <typename T, class C> inline T average(C &c) {
+  float a = 0;
+  float scalar = (c.size() / 32.0);
+  for (auto b : c) {
+    a += b * (scalar < 1 ? 1 : scalar);
+  }
   return a / c.size();
 }
 
-
+template <typename T, class C> inline T max(C &c) {
+  T a = 0;
+  for (auto b : c) {
+    if (b > a) { a = b; }
+  }
+  return a;
+}
 
 /**
  * Calculate the gain for a given frequency.
@@ -117,45 +119,48 @@ inline T average(C &c) {
  * freq: frequency
  * fm: the mid-band frequency of the 1/b-octave filter
  */
-template <typename T>
-inline T gain(T band, T freq, T fm) {
-  return sqrt(1.0 / (1.0 + pow(((freq/fm) - (fm/freq)) * (1.507 * band), 6.0)));
+template <typename T> inline T calculate_gain(T band, T freq, T fm) {
+  return sqrt(1.0 /
+              (1.0 + pow(((freq / fm) - (fm / freq)) * (1.507 * band), 6.0)));
 }
 
-inline float upper_freq_bound(float mid, float band){
-    return mid * pow(sqrt(2), 1.0/band);
+inline float upper_freq_bound(float mid, float band) {
+  return mid * pow(sqrt(2), 1.0 / band);
 }
 
-inline float lower_freq_bound(float mid, float band){
-    return mid / pow(sqrt(2), 1.0/band);
+inline float lower_freq_bound(float mid, float band) {
+  return mid / pow(sqrt(2), 1.0 / band);
 }
 
-void populate_bound_arrays(){
-    for (uint i = 0; i < 32; i++){
-        lbtomf[i] = lower_freq_bound(tomf[i], 3);
-        ubtomf[i] = upper_freq_bound(tomf[i], 3);
-    }
+void populate_bound_arrays() {
+  for (uint i = 0; i < 32; i++) {
+    lbtomf[i] = lower_freq_bound(tomf[i], 3);
+    ubtomf[i] = upper_freq_bound(tomf[i], 3);
+  }
 }
+
+static unordered_map<int, int> freq_octave_map;
 
 // TODO: make this some sort of tree thing
 // Instead of an O(N) lookup
 int get_octave_bin(float freq) {
+  auto got = mymap.find(freq);
+  if ( got != mymap.end() ) {  // we found a freq -> octave map
+    return *got;               // so return the octave
+  } else {
     for (int i = 0; i < 3 * 11; i++) {
-        if (lbtomf[i] <= freq && freq < ubtomf[i]) {
-            return i;
-        }
+      if (lbtomf[i] <= freq && freq < ubtomf[i]) {
+        freq_octave_map[freq] = i;
+        return i;
+      }
     }
-    return -1;
-}
-
-template <typename T>
-vector<T>
-bin_freqs_to_octaves(array<T, FRAMES_PER_BUFFER> &freqs) {
-
+  }
+  return -1;
 }
 
 static void fftwProcess(const void *inputBuffer) {
-  if (inputBuffer == NULL) return;
+  if (inputBuffer == NULL)
+    return;
   float **input_ptr_ary = (float **)inputBuffer;
   float *left_in = input_ptr_ary[0];
   float *right_in = input_ptr_ary[1];
@@ -177,15 +182,19 @@ static void fftwProcess(const void *inputBuffer) {
 
 static int copyCallback(const void *inputBuffer, void *outputBuffer,
                         unsigned long framesPerBuffer,
-                        const PaStreamCallbackTimeInfo*,
-                        PaStreamCallbackFlags, void*) {
+                        const PaStreamCallbackTimeInfo *, PaStreamCallbackFlags,
+                        void *) {
   //    Copy stuff
-  if( inputBuffer == NULL) return 0;
-  memcpy(((float**)outputBuffer)[0], ((float**)inputBuffer)[0], framesPerBuffer * sizeof(float));
-  memcpy(((float**)outputBuffer)[1], ((float**)inputBuffer)[1], framesPerBuffer * sizeof(float));
-  //thread (fftwProcess, inputBuffer).detach();
+  if (inputBuffer == NULL)
+    return 0;
+  memcpy(((float **)outputBuffer)[0], ((float **)inputBuffer)[0],
+         framesPerBuffer * sizeof(float));
+  memcpy(((float **)outputBuffer)[1], ((float **)inputBuffer)[1],
+         framesPerBuffer * sizeof(float));
+  // thread (fftwProcess, inputBuffer).detach();
 
-  if (inputBuffer == NULL) return paContinue;
+  if (inputBuffer == NULL)
+    return paContinue;
   float **input_ptr_ary = (float **)inputBuffer;
   float *left_in = input_ptr_ary[0];
   float *right_in = input_ptr_ary[1];
@@ -202,7 +211,6 @@ static int copyCallback(const void *inputBuffer, void *outputBuffer,
 
   return paContinue;
 }
-
 
 void setupAudio(PaStream *stream) {
   PaStreamParameters inputParameters, outputParameters;
@@ -238,7 +246,7 @@ void setupAudio(PaStream *stream) {
                       /* paClipOff, */ /* we won't output out of range samples
                                           so don't bother clipping them */
                       copyCallback, NULL);
-    
+
   if (err != paNoError)
     Pa_Terminate();
 
@@ -247,28 +255,16 @@ void setupAudio(PaStream *stream) {
     Pa_Terminate();
 }
 
-/*******************************************************************/
-// int main(void) {
-//  PaStream *stream = nullptr;
-//  setupAudio(stream);
-//
-//  while (true) {
-//  };
-//
-//  Pa_CloseStream(stream);
-//  Pa_Terminate();
-//  std::cout << "Quiting!" << std::endl;
-//  return 0;
-//}
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow), stream(nullptr) {
   left_mid = (float *)fftwf_malloc(sizeof(float) * FRAMES_PER_BUFFER);
   right_mid = (float *)fftwf_malloc(sizeof(float) * FRAMES_PER_BUFFER);
-  lp = fftwf_plan_dft_r2c_1d(FRAMES_PER_BUFFER, left_mid, left_out, FFTW_MEASURE);
-  rp = fftwf_plan_dft_r2c_1d(FRAMES_PER_BUFFER, right_mid, right_out, FFTW_MEASURE);
+  lp = fftwf_plan_dft_r2c_1d(FRAMES_PER_BUFFER, left_mid, left_out,
+                             FFTW_MEASURE);
+  rp = fftwf_plan_dft_r2c_1d(FRAMES_PER_BUFFER, right_mid, right_out,
+                             FFTW_MEASURE);
   populate_bound_arrays();
-  
+
   ui->setupUi(this);
   setGeometry(400, 250, 542, 390);
 
@@ -288,23 +284,14 @@ MainWindow::~MainWindow() {
   delete ui;
 }
 
-
-
 void MainWindow::setupBarChart(QCustomPlot *customPlot) {
   demoName = "Bar Chart Demo";
   // create empty bar chart objects:
   QCPBars *fossil = new QCPBars(customPlot->xAxis, ui->customPlot->yAxis);
   customPlot->addPlottable(fossil);
-//  // set names and colors:
-//  QPen pen;
-//  pen.setWidthF(1.2);
-//  fossil->setName("Fossil fuels");
-//  pen.setColor(QColor(255, 131, 0));
-//  fossil->setPen(pen);
-//  fossil->setBrush(QColor(255, 131, 0, 50));
-  
-    // prepare y axis:
-  customPlot->yAxis->setRange(0, 65);
+
+  // prepare y axis:
+  customPlot->yAxis->setRange(0, 85);
   customPlot->yAxis->setPadding(5); // a bit more space to the left border
   customPlot->yAxis->setLabel("DB");
   customPlot->yAxis->grid()->setSubGridVisible(true);
@@ -314,7 +301,7 @@ void MainWindow::setupBarChart(QCustomPlot *customPlot) {
   customPlot->yAxis->grid()->setPen(gridPen);
   gridPen.setStyle(Qt::DotLine);
   customPlot->yAxis->grid()->setSubGridPen(gridPen);
-  
+
   customPlot->xAxis->setRange(0, 10);
   customPlot->xAxis->setPadding(5); // a bit more space to the left border
   customPlot->xAxis->setLabel("Hz");
@@ -324,73 +311,49 @@ void MainWindow::setupBarChart(QCustomPlot *customPlot) {
   customPlot->xAxis->grid()->setPen(gridPen);
   gridPen.setStyle(Qt::DotLine);
   customPlot->xAxis->grid()->setSubGridPen(gridPen);
-  
+
   // setup a timer that repeatedly calls MainWindow::realtimeDataSlot:
   connect(&dataTimer, SIGNAL(timeout()), this, SLOT(realtimeDataSlot()));
   dataTimer.start(0); // Interval 0 means to refresh as fast as possible
 }
 
-
-
 void MainWindow::realtimeDataSlot() {
   double key = QDateTime::currentDateTime().toMSecsSinceEpoch() / 1000.0;
 
   // get bar chart
-  QCPBars* fossil = static_cast<QCPBars*>(ui->customPlot->plottable(0));
+  QCPBars *fossil = static_cast<QCPBars *>(ui->customPlot->plottable(0));
   fossil->clearData(); // clear the current data
 
-//  vector<double> octaves;
-//  // 0 is DC freq (O Hz)
-//  // n/2 is nyquist freq
-//  for (uint i = 1; i < (FRAMES_PER_BUFFER / 2) - 1; i++) {
-//    //ticks << (i * SAMPLE_RATE / FRAMES_PER_BUFFER);
-//    auto a = abs(left_out[i][0]);
-//    auto b = gain<float>(<#T band#>, <#T freq#>, <#T fm#>)
-//    tempdata.push_back();
-//  }
+  // 0 is DC freq (O Hz)
+  // n/2 is nyquist freq
+  array<vector<float>, 32> octave_bins;
+  for (uint i = 2; i < (FRAMES_PER_BUFFER / 2) - 1; i++) {
+    float freq   = (i * SAMPLE_RATE / FRAMES_PER_BUFFER);
+    int   octave = get_octave_bin(freq);
+    float gain   = calculate_gain<float>(3, freq, tomf[octave]);
+    float val    = abs(left_out[i][0]);
+    octave_bins[octave].push_back(val * gain);
+  }
 
-    vector<vector<float>> octave_bins(32);
-    vector<float> octaves;
-    
-    for (uint i = 2; i < (FRAMES_PER_BUFFER / 2) - 1; i++) {
-        float freq = (i * SAMPLE_RATE / FRAMES_PER_BUFFER);
-        int octave = get_octave_bin(freq);
-        float gain_mult = gain<float>(3, freq, tomf[octave]);
-        float val = abs(left_out[i][0]);
-        octave_bins[octave].push_back(val * gain_mult);
+  // prepare x axis with labels:
+  QVector<double> ticks;
+  QVector<double> data;
+  uint i = 0;
+  for (vector<float> bin : octave_bins) {
+    ticks << i++;
+    double maxed = max<double>(bin);
+    if (isnan(maxed)) {
+      data.push_back(0);
+    } else if (maxed > 80) {
+      data.push_back(80);
+    } else {
+      data.push_back(maxed);
     }
-    
-    // prepare x axis with country labels:
-    QVector<double> ticks;
-    QVector<double> data;
-    uint i = 0;
-    for (vector<float> bin : octave_bins){
-        ticks << i++;
-        double averaged = average<double>(bin);
-        if (isnan(averaged)) {
-            data.push_back(0);
-        } else if (averaged > 60) {
-            data.push_back(60);
-        } else {
-            data.push_back(averaged);
-        }
-    }
+  }
 
-  
-//  uint i =2;
-//  for (auto it = tempdata.begin(); !(it >= tempdata.end()); it+=2) {
-//    vector<float> temp(it,it+2);
-//    ticks << (i * SAMPLE_RATE / FRAMES_PER_BUFFER);
-//    i+=2;
-//    data << average(temp);
-//  } 
-  
-
-
-  fossil->setData(ticks, data);  // add the new data
-  fossil->rescaleKeyAxis();      // scale the X axis
-  ui->customPlot->replot();      // redraw
-
+  fossil->setData(ticks, data); // add the new data
+  fossil->rescaleKeyAxis();     // scale the X axis
+  ui->customPlot->replot();     // redraw
 
   // calculate frames per second:
   static double lastFpsKey;
@@ -399,7 +362,7 @@ void MainWindow::realtimeDataSlot() {
   // average fps over 2 seconds
   if (key - lastFpsKey > 2) {
     ui->statusBar->showMessage(
-        QString("%1 FPS").arg(frameCount / (key - lastFpsKey), 0, 'f', 0),0);
+        QString("%1 FPS").arg(frameCount / (key - lastFpsKey), 0, 'f', 0), 0);
     lastFpsKey = key;
     frameCount = 0;
   }
@@ -408,4 +371,3 @@ void MainWindow::realtimeDataSlot() {
 void MainWindow::setupPlayground(QCustomPlot *customPlot) {
   Q_UNUSED(customPlot)
 }
-
